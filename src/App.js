c6438bb2f331dcf88a6ee2daf1cee311332a6d9e
ipatch <chris.r.jones.1983@gmail.com>
2017-12-05 22:16:58 -0600

    convert `App.js` to a stateless functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import Hello from './hello.js';
-// import Diana from './diana.js';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Hello />
-        {/* <Diana /> */}
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Hello />
+  </div>
+);
 
 export default App;
